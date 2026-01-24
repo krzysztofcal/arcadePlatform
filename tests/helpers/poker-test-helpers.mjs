739d1c03f1abb6aed1ec727e482c913121f9b0dc
krzysztofcal <31594949+krzysztofcal@users.noreply.github.com>
2026-01-24 08:52:51 +0100

    Remove poker test global

--- tests/helpers/poker-test-helpers.mjs
+++ tests/helpers/poker-test-helpers.mjs
@@ -1,9 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
-import { isValidTwoCards } from "../../netlify/functions/_shared/poker-cards-utils.mjs";
 
 const root = process.cwd();
-if (!globalThis.isValidTwoCards) globalThis.isValidTwoCards = isValidTwoCards;
 
 const stripImports = (source) => source.replace(/^\s*import[\s\S]*?;\s*$/gm, "");
 
@@ -49,6 +47,7 @@ export const loadPokerHandler = (filePath, mocks) => {
     "getRng",
     "isPlainObject",
     "isStateStorageValid",
+    "isValidTwoCards",
     "klog",
     "normalizeJsonState",
     "normalizeRequestId",
